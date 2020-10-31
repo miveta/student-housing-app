1a6f700bc5d05efe45933beb7d6c4f0528fab430
Mateja <mateja.iveta@fer.hr>
2020-10-31 23:57:40 +0100

    Promjena koda za spajanje na bazu - iz opreza!

---
+++ src/main/java/progi/projekt/config/DatabaseConfig.java
@@ -0,0 +1,17 @@
+package progi.projekt.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+/*    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }*/
+}
