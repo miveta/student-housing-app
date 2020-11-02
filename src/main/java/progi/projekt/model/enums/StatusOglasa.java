ff83527fcee9473b998aca50cf2f95dd1075f2de
denisd456 <dd51778@fer.hr>
2020-11-02 16:59:46 +0100

    dodani enumeratori

--- src/main/java/progi/projekt/model/enums/StatusOglasa.java
+++ src/main/java/progi/projekt/model/enums/StatusOglasa.java
@@ -1,5 +1,10 @@
 package progi.projekt.model.enums;
 
+//AKTIVAN- trenutno trazi nekog za potvrdu
+//OBRISAN- korisnik je obrisao ovaj oglas
+//ARHIVIRAN- oglas je iz prijasnih godina, vise nije validan. Mozda se nece koristiti
+//POTVRDEN- neki student je potvrdio ovaj oglas, te se ceka da zaposlenik SC-a potvrdi zamjenu
+//IZVEDEN- oglas je proveden od strane SC-a
 public enum StatusOglasa {
-    AKTIVAN, OBRISAN, ARHIVIRAN
+    AKTIVAN, OBRISAN, ARHIVIRAN, POTVRDEN, IZVEDEN
 }
---
+++ src/main/java/progi/projekt/model/enums/oznakeKategorija.java
@@ -0,0 +1,6 @@
+package progi.projekt.model.enums;
+
+//Neznam ih sve napamet, ovo su samo neke u rimskim brojevima
+public enum oznakeKategorija {
+    I, II, III, IV
+}
