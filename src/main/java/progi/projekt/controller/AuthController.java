5eb4d0b662c5a272d9278edb70129e50cf7f90c9
mholik <mh51680@fer.hr>
2020-11-13 00:42:13 +0100

    lokalizacija, enkapsulacija u KorisnikUserDetailsService,

--- src/main/java/progi/projekt/controller/AuthController.java
+++ src/main/java/progi/projekt/controller/AuthController.java
@@ -30,31 +30,32 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final KorisnikUserDetailsService korisnikUserDetailsService;
+	private final KorisnikUserDetailsService korisnikUserDetailsService;
 
-    private final AuthenticationHandler authenticationHandler;
+	private final AuthenticationHandler authenticationHandler;
 
-    private final StudentService studentService;
+	private final StudentService studentService;
 
-    private final ZaposlenikSCService zaposlenikscService;
+	private final ZaposlenikSCService zaposlenikscService;
 
-    private final PasswordEncoder pswdEncoder;
+	private final PasswordEncoder pswdEncoder;
 
-    public AuthController(KorisnikUserDetailsService korisnikUserDetailsService,
-                          AuthenticationHandler authenticationHandler, StudentService studentService,
-                          ZaposlenikSCService zaposlenikscService, PasswordEncoder pswdEncoder) {
-        this.korisnikUserDetailsService = korisnikUserDetailsService;
-        this.authenticationHandler = authenticationHandler;
-        this.studentService = studentService;
-        this.zaposlenikscService = zaposlenikscService;
-        this.pswdEncoder = pswdEncoder;
-    }
+	public AuthController(KorisnikUserDetailsService korisnikUserDetailsService,
+						  AuthenticationHandler authenticationHandler, StudentService studentService,
+						  ZaposlenikSCService zaposlenikscService, PasswordEncoder pswdEncoder) {
+		this.korisnikUserDetailsService = korisnikUserDetailsService;
+		this.authenticationHandler = authenticationHandler;
+		this.studentService = studentService;
+		this.zaposlenikscService = zaposlenikscService;
+		this.pswdEncoder = pswdEncoder;
+	}
 
-    //'@AuthenticationPrincipal WebRequest request' je argument za citanje cijelog requesta
-    //'@RequestBody LoginForm loginForm' je argument za citanje cijelog bodya requesta
-    //'@AuthenticationPrincipal User userLoginData' vraca podatke o korisniku u security contextu
+	//'@AuthenticationPrincipal WebRequest request' je argument za citanje cijelog requesta
+	//'@RequestBody LoginForm loginForm' je argument za citanje cijelog bodya requesta
+	//'@AuthenticationPrincipal User userLoginData' vraca podatke o korisniku u security contextu
 
-    // not currently used
+	//region Backend testiranje sessiona
+	// not currently used
     /*
     @GetMapping("/userInfo")
     public ResponseEntity<?> showLoggedInUser(@AuthenticationPrincipal User loggedInUser) {
@@ -69,66 +70,69 @@ public class AuthController {
         return ResponseEntity.ok(studentDTO);
     }
     */
+	//endregion
 
-    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> register(@RequestBody @Validated RegisterForm registerForm) {
-        if (studentService.studentExists(registerForm.getUsername())) {
-            return ResponseEntity.badRequest().body("Već postoji korisnik s tim imenom!");
-        }
+	@PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> register(@RequestBody @Validated RegisterForm registerForm) {
+		if (studentService.studentExists(registerForm.getUsername())) {
+			return ResponseEntity.badRequest().body("Već postoji korisnik s tim imenom!");
 
-        // ne provjeravamo zaposlenike zato što se oni unose u bazu preko backenda
+		}
 
-        //izrada Student modela
-        String passhash = pswdEncoder.encode(registerForm.getLozinka());
-        Student student = registerForm.fromRegisterForm(passhash);
-        student = studentService.createStudent(student);
+		// ne provjeravamo zaposlenike zato što se oni unose u bazu preko backenda
 
+		//izrada Student modela
+		String passhash = pswdEncoder.encode(registerForm.getLozinka());
+		Student student = registerForm.fromRegisterForm(passhash);
+		student = studentService.createStudent(student);
 
-        //login nakon registracije
-        LoginForm data = new LoginForm(registerForm.getUsername(), registerForm.getLozinka());
-        Authentication auth = authenticationHandler.authenticate(data);
-        SecurityContext sc = SecurityContextHolder.getContext();
-        sc.setAuthentication(auth);
+		//login nakon registracije
+		LoginForm data = new LoginForm(registerForm.getUsername(), registerForm.getLozinka());
+		Authentication auth = authenticationHandler.authenticate(data);
+		SecurityContext sc = SecurityContextHolder.getContext();
+		sc.setAuthentication(auth);
 
-        KorisnikDTO studentDTO = new KorisnikDTO(student);
-        return ResponseEntity.ok(studentDTO);
-    }
+		KorisnikDTO studentDTO = new KorisnikDTO(student);
+		return ResponseEntity.ok(studentDTO);
+	}
 
-    /*
-    //front mora poslati 'loginForm' objekt, controller vraca 200 za dobre podatke, 401 za lose
-    //Header: Content-Type=application/json
-    //body:
-        {
-            "username": "user",
-            "password": "pass"
-        }
-    */
-    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> login(@RequestBody LoginForm data) {
-        try {
-            Authentication auth = authenticationHandler.authenticate(data);
+	/*
+	//front mora poslati 'loginForm' objekt, controller vraca 200 za dobre podatke, 401 za lose
+	//body responsa je orignalna poruka o gresci
+	//postman:
+	//Header: Content-Type=application/json
+	//body:
+		{
+			"username": "user",
+			"password": "pass"
+		}
+	*/
+	@PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> login(@RequestBody LoginForm data) {
+		try {
+			Authentication auth = authenticationHandler.authenticate(data);
 
-            SecurityContext sc = SecurityContextHolder.getContext();
-            sc.setAuthentication(auth);
+			SecurityContext sc = SecurityContextHolder.getContext();
+			sc.setAuthentication(auth);
 
-            //nije potrebno dok koristimo Springove sessione
-            //HttpSession session = req.getSession(true);
-            //session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
+			//nije potrebno dok koristimo Springove sessione
+			//HttpSession session = req.getSession(true);
+			//session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
 
-            //if successful
-            // authentication handler will throw a badcredentials exception if the student doesnt exist before it reaches this step
-            Korisnik korisnik;
+			//if successful
+			// authentication handler will throw a badcredentials exception if the student doesnt exist before it reaches this step
+			Korisnik korisnik;
 
-            Optional<Student> student = studentService.findBykorisnickoIme(data.getUsername());
-            korisnik = student.isPresent() ? student.get() : zaposlenikscService.findBykorisnickoIme(data.getUsername()).get();
+			Optional<Student> student = studentService.findBykorisnickoIme(data.getUsername());
+			korisnik = student.isPresent() ? student.get() : zaposlenikscService.findBykorisnickoIme(data.getUsername()).get();
 
-            KorisnikDTO korisnikDTO = new KorisnikDTO(korisnik);
+			KorisnikDTO korisnikDTO = new KorisnikDTO(korisnik);
 
-            return ResponseEntity.ok(korisnikDTO);
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
+			return ResponseEntity.ok(korisnikDTO);
+		} catch (BadCredentialsException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
 }
--- src/main/java/progi/projekt/security/KorisnikUserDetailsService.java
+++ src/main/java/progi/projekt/security/KorisnikUserDetailsService.java
@@ -18,37 +18,23 @@ import java.util.Optional;
 
 @Service
 public class KorisnikUserDetailsService implements UserDetailsService {
-    @Value("${progi.projekt.admin.pass}")
-    private String adminPasswordHash;
+	@Value("${progi.projekt.admin.pass}")
+	private String adminPasswordHash;
 
-    private final StudentService studentService;
+	private final StudentService studentService;
 
-    private final ZaposlenikSCService zaposlenikscService;
+	private final ZaposlenikSCService zaposlenikscService;
 
-    public KorisnikUserDetailsService(StudentService studentService, ZaposlenikSCService zaposlenikscService) {
-        this.studentService = studentService;
-        this.zaposlenikscService = zaposlenikscService;
-    }
+	public KorisnikUserDetailsService(StudentService studentService, ZaposlenikSCService zaposlenikscService) {
+		this.studentService = studentService;
+		this.zaposlenikscService = zaposlenikscService;
+	}
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        //try catch se moze rjesiti i preko optional klasa
-        try {
-            Optional<Student> student = studentService.findBykorisnickoIme(username);
-            if (student.isPresent()) {
-                String rolesStudent = "ROLE_STUDENT, ROLE_USER";
-                return new User(student.get().getKorisnickoIme(), student.get().getLozinka(), AuthorityUtils.commaSeparatedStringToAuthorityList(rolesStudent));
-            } else {
-                Optional<ZaposlenikSC> zaposlenikSC = zaposlenikscService.findBykorisnickoIme(username);
-                if (zaposlenikSC.isPresent()) {
-                    String rolesZaposlenik = "ROLE_ADMIN, ROLE_ZAPOSLENIKSC, ROLE_STUDENT, ROLE_USER";
-                    return new User(zaposlenikSC.get().getKorisnickoIme(), zaposlenikSC.get().getLozinka(), AuthorityUtils.commaSeparatedStringToAuthorityList(rolesZaposlenik));
-                } else {
-                    throw new UsernameNotFoundException("No user '" + username + "'");
-                }
-            }
-
-/*
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		try {
+			//region privremeni hardcoded login koji zaobidje bazu
+			/*
             //privremeni hardcoded login koji zaobidje bazu
             if (username.equals("admin")) {
                 String rolesZaposlenik = "ROLE_ADMIN, ROLE_ZAPOSLENIKSC, ROLE_STUDENT, ROLE_USER";
@@ -61,65 +47,65 @@ public class KorisnikUserDetailsService implements UserDetailsService {
             } else {
                 throw new UsernameNotFoundException("No user '" + username + "'");
             }
- */
-            //return new User(username, password(username), authorities(username));
-        } catch (Exception e) {
-            //ovo ce uloviti exception od password() i authorities(), i ako constructor od Usera nesto baci
-            throw new UsernameNotFoundException("No user '" + username + "'");
-        }
+ 			*/
+			//endregion
 
-    }
+			return new User(username, password(username), authorities(username));
+		} catch (Exception e) {
+			//ovo ce uloviti exception od password() i authorities(), i ako constructor od Usera nesto baci
+			throw new UsernameNotFoundException("Nema korisnika '" + username + "'");
+		}
 
-    private String password(String username) throws UsernameNotFoundException {
-        if (username.equals("admin"))
-            return adminPasswordHash;
+	}
 
-        //ako login nije hardcoded "admin", password dobijemo iz DB-a:
-        try {
-            Optional<Student> student = studentService.findBykorisnickoIme(username);
-            if (student.isEmpty()) {
-                Optional<ZaposlenikSC> zaposlenik = zaposlenikscService.findBykorisnickoIme(username);
-                if (zaposlenik.isEmpty()) {
-                    throw new UsernameNotFoundException("No user '" + username + "'");
-                } else {
-                    return zaposlenikscService.getLozinka(zaposlenik.get());
-                }
-            } else {
-                return studentService.getLozinka(student.get());
-            }
+	private String password(String username) throws UsernameNotFoundException {
+		if (username.equals("admin"))
+			return adminPasswordHash;
 
-        } catch (UsernameNotFoundException e) {
-            String porukaIzStudSerImpl = e.getMessage();
-            throw new UsernameNotFoundException("No user '" + username + "'");
-            //ovoga ulovi SecurityExceptionHandler
-        }
-    }
+		//ako login nije hardcoded "admin", password dobijemo iz DB-a:
+		try {
+			Optional<Student> student = studentService.findBykorisnickoIme(username);
+			if (student.isEmpty()) {
+				Optional<ZaposlenikSC> zaposlenik = zaposlenikscService.findBykorisnickoIme(username);
+				if (zaposlenik.isEmpty()) {
+					throw new UsernameNotFoundException("Nema korisnika '" + username + "'");
+				} else {
+					return zaposlenikscService.getLozinka(zaposlenik.get());
+				}
+			} else {
+				return studentService.getLozinka(student.get());
+			}
 
-    private List<GrantedAuthority> authorities(String username) throws UsernameNotFoundException {
-        if (username.equals("admin"))
-            return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN");
+		} catch (UsernameNotFoundException e) {
+			String porukaIzStudSerImpl = e.getMessage();
+			throw new UsernameNotFoundException("Nema korisnika '" + username + "'");
+		}
+	}
 
-        //ako login nije hardcoded "admin", role dobijemo iz DB-a:
-        try {
-            Optional<Student> student = studentService.findBykorisnickoIme(username);
-            if (student.isEmpty()) {
-                Optional<ZaposlenikSC> zaposlenik = zaposlenikscService.findBykorisnickoIme(username);
-                if (zaposlenik.isEmpty()) {
-                    throw new UsernameNotFoundException("No user '" + username + "'");
-                } else {
-                    String rolesZaposlenik = "ROLE_ADMIN, ROLE_ZAPOSLENIKSC, ROLE_STUDENT, ROLE_USER";
-                    return AuthorityUtils.commaSeparatedStringToAuthorityList(rolesZaposlenik);
-                }
-            } else {
-                String rolesStudent = "ROLE_STUDENT, ROLE_USER";
-                return AuthorityUtils.commaSeparatedStringToAuthorityList(rolesStudent);
-            }
+	private List<GrantedAuthority> authorities(String username) throws UsernameNotFoundException {
+		if (username.equals("admin"))
+			return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN, ROLE_ZAPOSLENIKSC");
 
-        } catch (UsernameNotFoundException e) {
-            String porukaIzStudSerImpl = e.getMessage();
-            throw new UsernameNotFoundException("No user '" + username + "'");
-            //ovoga ulovi SecurityExceptionHandler
-        }
-    }
+		//ako login nije hardcoded "admin", role dobijemo iz DB-a:
+		try {
+			Optional<Student> student = studentService.findBykorisnickoIme(username);
+			if (student.isEmpty()) {
+				Optional<ZaposlenikSC> zaposlenik = zaposlenikscService.findBykorisnickoIme(username);
+				if (zaposlenik.isEmpty()) {
+					throw new UsernameNotFoundException("Nema korisnika '" + username + "'");
+				} else {
+					String rolesZaposlenik = "ROLE_ADMIN, ROLE_ZAPOSLENIKSC, ROLE_STUDENT, ROLE_USER";
+					return AuthorityUtils.commaSeparatedStringToAuthorityList(rolesZaposlenik);
+				}
+			} else {
+				String rolesStudent = "ROLE_STUDENT, ROLE_USER";
+				return AuthorityUtils.commaSeparatedStringToAuthorityList(rolesStudent);
+			}
+
+		} catch (UsernameNotFoundException e) {
+			String porukaIzStudSerImpl = e.getMessage();
+			throw new UsernameNotFoundException("Nema korisnika '" + username + "'");
+		}
+	}
 
 }
