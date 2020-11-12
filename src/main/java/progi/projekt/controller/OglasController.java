5dc8cf620a9701fd0e1b11b36b886a52488cbdd8
Mateja <mateja.iveta@fer.hr>
2020-11-12 11:24:59 +0100

    popravljen cors error za oglas/list

--- src/main/java/progi/projekt/controller/OglasController.java
+++ src/main/java/progi/projekt/controller/OglasController.java
@@ -1,14 +1,15 @@
 package progi.projekt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import progi.projekt.dto.OglasDTO;
 import progi.projekt.model.Oglas;
-import progi.projekt.repository.OglasRepository;
 import progi.projekt.service.OglasService;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,7 +20,7 @@ public class OglasController {
     @Autowired
     private OglasService oglasService;
 
-    @GetMapping("")
+    @GetMapping("/list")
     public List<OglasDTO> listOglas() {
         List<Oglas> oglasi = oglasService.listAll();
         List<OglasDTO> oglasiDTO = oglasi.stream().map(o -> new OglasDTO(o)
