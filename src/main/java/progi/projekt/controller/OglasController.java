f231021f2d88c2a2cef2b90caf1ca9290740f20a
mholik <mh51680@fer.hr>
2021-01-07 12:27:13 +0100

    listKandidati

--- src/main/java/progi/projekt/controller/OglasController.java
+++ src/main/java/progi/projekt/controller/OglasController.java
@@ -4,10 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import progi.projekt.dto.KandidatDTO;
 import progi.projekt.dto.OglasDTO;
+import progi.projekt.model.Kandidat;
+import progi.projekt.model.Lajk;
 import progi.projekt.model.Oglas;
+import progi.projekt.service.KandidatService;
+import progi.projekt.service.LajkService;
 import progi.projekt.service.OglasService;
+import progi.projekt.service.util.Key;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,9 +22,13 @@ public class OglasController {
 
 
     private OglasService oglasService;
+    private LajkService lajkService;
+    private KandidatService kandidatService;
 
-    public OglasController(OglasService oglasService) {
+    public OglasController(OglasService oglasService, LajkService lajkService, KandidatService kandidatService) {
         this.oglasService = oglasService;
+        this.lajkService = lajkService;
+        this.kandidatService = kandidatService;
     }
 
     @GetMapping("/list")
@@ -35,6 +44,38 @@ public class OglasController {
 
     @GetMapping(value = "/listKandidati")
     public List<KandidatDTO> listKandidati(@RequestParam(value = "oglas_id") String oglasId) {
-        return null;
+        Optional<Oglas> oglasOpt = oglasService.findById(oglasId);
+        ArrayList<KandidatDTO> kandidatiDTO = new ArrayList<KandidatDTO>();
+
+        if (oglasOpt.isPresent()){
+            Oglas oglas = oglasOpt.get();
+            UUID studentID = oglas.getStudent().getId();
+            List<Lajk> lajkovi = lajkService.listAll();
+            for (Lajk lajk : lajkovi) {
+                if (lajk.getLajkId().getOglas().getId() == oglas.getId()) {
+                    Optional<Integer> ocjenaOptional = Optional.ofNullable(lajk.getOcjena());
+                    Kandidat tmp = kandidatService.kandidatParaOglasa(oglas, lajk.getLajkId().getOglas()).get();
+                    KandidatDTO tmpDTO = new KandidatDTO(tmp);
+
+                    ocjenaOptional.ifPresentOrElse(
+                            (ocjena) ->
+                            {
+                                tmpDTO.setKandOcjena(ocjena);
+                            },
+                            () ->
+                            {
+                                //ako ocjena jos nije unesena upisujemo -1
+                                tmpDTO.setKandOcjena(-1);
+                            });
+
+                    kandidatiDTO.add(tmpDTO);
+                }
+            }
+            return kandidatiDTO;
+        }
+
+        else {
+            return new ArrayList<KandidatDTO>();
+        }
     }
 }
