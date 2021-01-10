e08c69c68eb9e46a7b32b2083f63059d09df2b3f
mateja <mi51347@fer.hr>
2021-01-10 19:10:33 +0100

    bugfix - soba odabir paviljona

--- front/src/components/ObavijestiDropdown.jsx
+++ front/src/components/ObavijestiDropdown.jsx
@@ -58,9 +58,7 @@ class ObavijestiDropdown extends Component {
                 <SockJsClient url='http://localhost:8080/websocket-chat/'
                               topics={['/topic/user']}
                               onConnect={() => {
-                                  console.log(this.clientRef)
                                   this.getObavijesti()
-                                  console.log("connected");
                               }}
                               onDisconnect={() => {
                                   console.log("Disconnected");
