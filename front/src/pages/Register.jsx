490ad94cc9c746af7b4e823a2eb322302d6a4df7
Dora Horvat <dora.dorotea@gmail.com>
2021-01-12 18:44:02 +0000

    Ispravak min i max length za JMBAG

--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -118,12 +118,12 @@ class Register extends Component {
                         <Form.Label> Prezime </Form.Label>
                         <Form.Control name="prezime" type="text" placeholder={this.state.prezime}
                                       onChange={this.onChange} required
-                                      maxLength="10" minLength="10"/>
+                                      />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label> JMBAG </Form.Label>
                         <Form.Control name="jmbag" type="text" placeholder={this.state.jmbag} onChange={this.onChange}
-                                      required/>
+                                      required maxLength="10" minLength="10"/>
                         <Form.Text className={"textMuted"}>
                             JMBAG se sastoji od 10 znamenki!
                         </Form.Text>
